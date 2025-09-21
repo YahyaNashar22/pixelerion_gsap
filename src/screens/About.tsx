9fef87b170c62682b52e7a956e2e4e443e9ea755
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-09-21 22:55:22 +0300

    started about us page

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,5 +1,26 @@
+// --white: #ffffff;
+// --black: #000000;
+// --yellow: #ffd700;
+// --red: #ff3131;
+// --blue: #00a8e8;
+// --green: #7de247;
+// --grid: #909090;
+
+const AboutContent = [
+  {
+    id: "1",
+    title: "Creative & Functional Design",
+    text: "At Pixelerion, we transform digital ideas into vibrant, functional experiences. With a passionate team and a strong focus on design, code, and collaboration, we help brands of all sizes create products that truly stand out.",
+    color: "var(--yellow)",
+  },
+];
+
 const About = () => {
-  return <section id="about" className="screen">About</section>;
+  return (
+    <section id="about" className="screen">
+      About
+    </section>
+  );
 };
 
 export default About;
